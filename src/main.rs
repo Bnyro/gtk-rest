7da68f43650a2207b16ca55cb69521958f03a36e
Bnyro <bnyro@tutanota.com>
2022-11-29 18:05:52 +0100

    add a border radius to the sourcview

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,11 @@ mod utils;
 mod widgets;
 mod window;
 use application::GtkRestApplication;
+use gtk::gdk::Display;
 use gtk::gio;
 use gtk::prelude::*;
+use gtk::CssProvider;
+use gtk::StyleContext;
 use window::Window;
 
 const APP_ID: &str = "com.bnyro.rest";
@@ -19,9 +22,25 @@ fn main() {
     // desktop features such as file opening and single-instance applications.
     let app = GtkRestApplication::new(APP_ID, &gio::ApplicationFlags::empty());
 
+    // Attach the CSS when the app is loaded
+    app.connect_startup(|_| load_css());
+
     // Run the application. This function will block until the application
     // exits. Upon return, we have our exit code to return to the shell. (This
     // is the code you see when you do `echo $?` after running a command in a
     // terminal.
     std::process::exit(app.run());
+}
+
+fn load_css() {
+    // Load the CSS file and add it to the provider
+    let provider = CssProvider::new();
+    provider.load_from_data(include_bytes!("style.css"));
+
+    // Add the provider to the default screen
+    StyleContext::add_provider_for_display(
+        &Display::default().expect("Could not connect to a display."),
+        &provider,
+        gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
+    );
 }
